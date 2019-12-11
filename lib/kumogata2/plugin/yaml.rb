ebcc68a8cca68feb21ead15d6a1af0192d783852
labocho <labocho@penguinlab.jp>
2019-12-11 14:42:44 +0900

    Support YAML tags like !Ref

--- lib/kumogata2/plugin/yaml.rb
+++ lib/kumogata2/plugin/yaml.rb
@@ -3,6 +3,18 @@ require 'yaml'
 class Kumogata2::Plugin::YAML
   Kumogata2::Plugin.register(:yaml, ['yaml', 'yml', 'template'], self)
 
+  # Register tags to parse abbreviated function call like `!Ref: Foo`
+  # See: https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference.html
+  %w(And Base64 Cidr Equals FindInMap GetAtt GetAZs If ImportValue Join Not Or Select Split Sub Transform).each do |tag|
+    YAML.add_domain_type("", tag) do |type, value|
+      {"Fn::" + tag => value}
+    end
+  end
+
+  YAML.add_domain_type("", "Ref") do |type, value|
+    {"Ref" => value}
+  end
+
   def initialize(options)
     @options = options
   end
